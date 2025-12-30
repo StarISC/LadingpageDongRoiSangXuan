d3643fb1a7757e9b901ab13cb745aa0e592511a0
tamnt88 <122974558+tamnt88@users.noreply.github.com>
2025-12-30 15:46:52 +0700

    cmd

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+﻿$(function () {
+  AOS.init({
+    duration: 800,
+    once: true,
+    offset: 120
+  });
+
+  new Swiper('.amenohashi', {
+    loop: true,
+    spaceBetween: 20,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false
+    }
+  });
+
+  const $backToTop = $('.back-to-top');
+  $(window).on('scroll', function () {
+    if ($(this).scrollTop() > 400) {
+      $backToTop.fadeIn();
+    } else {
+      $backToTop.fadeOut();
+    }
+  });
+
+  $backToTop.on('click', function () {
+    $('html, body').animate({ scrollTop: 0 }, 600);
+  });
+
+  $('.navbar .nav-link, .btn-cta').on('click', function (event) {
+    const target = $(this).attr('href');
+    if (target && target.startsWith('#')) {
+      event.preventDefault();
+      const offset = $(target).offset().top - 72;
+      $('html, body').animate({ scrollTop: offset }, 600);
+    }
+  });
+});
